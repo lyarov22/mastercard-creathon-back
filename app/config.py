db6dbc4e37714223543a376305044dc315183479
Your Name <you@example.com>
2025-11-15 09:50:50 +0000

    v2

--- app/config.py
+++ app/config.py
@@ -1,1 +1,9 @@
-DATABASE_URL = "postgresql+psycopg2://user:pass@popka:5432/mydb"
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+LLM_API_KEY = os.getenv("LLM_API_KEY")
+LLM_API_URL = os.getenv("LLM_API_URL")
--- app/main.py
+++ app/main.py
@@ -3,15 +3,32 @@ from db import ReadOnlyDB
 from models import Transaction
 import csv
 
-db = ReadOnlyDB()
+def get_shymkent():
 
-stmt = select(Transaction).where(Transaction.merchant_city == "Shymkent").limit(1)
+    db = ReadOnlyDB()
 
-with db.Session() as session:
-    result = session.execute(stmt).scalars().all()  # вернёт только 1 запись
-    if result:
-        with open("shymkent_transactions3.csv", "w", newline='', encoding='utf-8') as csvfile:
-            fieldnames = result[0].__table__.columns.keys()
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({col: getattr(result[0], col) for col in fieldnames})
+    stmt = select(Transaction).where(Transaction.merchant_city == "Shymkent").limit(1)
+
+    with db.Session() as session:
+        result = session.execute(stmt).scalars().all()  # вернёт только 1 запись
+        if result:
+            with open("shymkent_transactions3.csv", "w", newline='', encoding='utf-8') as csvfile:
+                fieldnames = result[0].__table__.columns.keys()
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow({col: getattr(result[0], col) for col in fieldnames})
+    print("done")
+
+
+def test_gemini():
+    from google import genai
+
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    )
+    print(response.text)
+
+test_gemini()
